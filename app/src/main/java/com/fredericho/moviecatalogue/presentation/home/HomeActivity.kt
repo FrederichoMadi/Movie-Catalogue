21ac0ac0f59f8c1a4788b0e9c9bbd01245a629f7
FrederichoMadi <frederichomadi@gmail.com>
2021-07-06 11:43:58 +0800

    sixth push

--- app/src/main/java/com/fredericho/moviecatalogue/presentation/home/HomeActivity.kt
+++ app/src/main/java/com/fredericho/moviecatalogue/presentation/home/HomeActivity.kt
@@ -1,9 +1,13 @@
 package com.fredericho.moviecatalogue.presentation.home
 
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.fredericho.moviecatalogue.R
@@ -13,6 +17,8 @@ import com.fredericho.moviecatalogue.presentation.home.ui.home.HomeFragment
 class HomeActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityHomeBinding
+
+    private lateinit var broadcastReceiver : BroadcastReceiver
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -51,5 +57,37 @@ class HomeActivity : AppCompatActivity() {
         image.setImageResource(resource)
     }
 
+    private fun registerBroadcastReceiver(){
+        broadcastReceiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context?, intent: Intent?) {
+                when(intent?.action){
+                    Intent.ACTION_POWER_CONNECTED -> {
+                        Toast.makeText(this@HomeActivity, "Power Connected", Toast.LENGTH_SHORT).show()
+                    }
+                    Intent.ACTION_POWER_DISCONNECTED -> {
+                        Toast.makeText(this@HomeActivity, "Power Disconnected", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
 
+        }
+        val intentFilter = IntentFilter()
+        intentFilter.apply {
+            addAction(Intent.ACTION_POWER_CONNECTED)
+            addAction(Intent.ACTION_POWER_DISCONNECTED)
+        }
+
+        registerReceiver(broadcastReceiver, intentFilter)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerBroadcastReceiver()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(broadcastReceiver)
+    }
 }
